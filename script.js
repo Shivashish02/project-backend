e2dbecf96f9b106053214bee5379b619985576be
Shivashish <shivashishsinghrawat02@gmail.com>
2022-05-13 15:03:28 +0530

    commit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const ImageModel = require('./models/image')
+const cors = require('cors');
+/*app.use(cors({
+    origin: 'https://www.section.io'
+}));*/
 
 const app = express()
 app.use(express.json())
@@ -10,20 +14,43 @@ app.use(express.urlencoded({ extended: true }))
 mongoose.connect('mongodb://localhost/test')
 
 
-app.get('/api/getimages', async (req, res) => {
-	const record = await ImageModel.findOne({pname : req.params.pname})
+
+
+app.get('/api/getimages/:pname', async (req, res) => {
+	const record = await ImageModel.findOne({ pname: req.params.pname })
 	console.log(record)
-	res.json(record)
+	if(record==null){
+		res.json(record)
+	}
+	else{
+		res.json(record.imageurl)
+	}
+	
 })
 
 app.post('/api/storeimageurl', async (req, res) => {
-	const record = req.body
-	const response = new ImageModel(record)
-	response.save()
+	const record = await ImageModel.findOne({ pname: req.body.pname })
+	if (record != null) {
+		ImageModel.findOneAndUpdate(
+			{ pname: req.body.pname },
+			{ $push: { imageurl: req.body.imageurl[0] } },
+			function (error, success) {
+				if (error) {
+					console.log(error);
+				} else {
+					console.log(success);
+				}
+			});
+	}
+	else {
+		const record = req.body
+		const response = new ImageModel(record)
+		response.save()
+	}
 	res.json({ status: 'ok' })
 })
 
 
-app.listen(3000, '127.0.0.1', () => {
+app.listen(4000, '127.0.0.1', () => {
 	console.log('Server up')
 })
